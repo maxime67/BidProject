7b6bd3382843356672a05bbf871f6d695dd6dd3b
Jules SENOT <jules.senot2023@campus-eni.fr>
2024-03-11 11:20:47 +0100

    commit initial

---
+++ src/test/java/com/grp3/bid/BidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp3.bid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
