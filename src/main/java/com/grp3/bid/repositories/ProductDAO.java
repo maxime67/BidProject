85e91b573d6c72a03b445e77698bc5eae5805e95
Thomas DELON <thomas.delon2023@campus-eni.fr>
2024-03-14 11:48:47 +0100

    update master product rowMapper

--- src/main/java/com/grp3/bid/repositories/ProductDAO.java
+++ src/main/java/com/grp3/bid/repositories/ProductDAO.java
@@ -88,7 +88,7 @@ public class ProductDAO implements ProductDAOInterface{
             Timestamp timestampStart = rs.getTimestamp("start_date");
             p.setStartDate(timestampStart.toLocalDateTime());
             Timestamp timestampEnd = rs.getTimestamp("end_date");
-            p.setStartDate(timestampEnd.toLocalDateTime());
+            p.setEndDate(timestampEnd.toLocalDateTime());
             p.setSeller(userDAO.getUserById(rs.getInt("id_seller")));
             p.setCategory(categoryDAO.getById(rs.getLong("category_id")));
             return p;
